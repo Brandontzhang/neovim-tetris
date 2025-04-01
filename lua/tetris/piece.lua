e31ddc51907df6170aa97d13faa584488d684c81
Brandon Zhang <zhangtbrandon2@gmail.com>
2025-04-01 15:37:24 -0400

    Rename from rotation to grid

--- lua/tetris/piece.lua
+++ lua/tetris/piece.lua
@@ -16,7 +16,7 @@ function Piece:setupGridDimensions(type)
 	self.type = type
 
 	-- Height and width of table holding the piece, including +1 on edges for collision checking
-	-- The row and col represent where the top left corner of the piece rotation is located on the grid. Adjusted to center on spawn
+	-- The row and col represent where the top left corner of the piece grid is located on the grid. Adjusted to center on spawn
 	self.row = 1
 	self.col = 4
 
@@ -34,54 +34,54 @@ end
 
 -- INFO: Follows SRS (Super Rotation System)
 function Piece:initGridValues()
-	self.rotation = {}
+	self.grid = {}
 
 	if self.type == nil then
 		error("Piece type not set")
 	end
 
 	for row = 1, self.height do
-		self.rotation[row] = {}
+		self.grid[row] = {}
 		for col = 1, self.width do
-			self.rotation[row][col] = 0 -- Fill with 0
+			self.grid[row][col] = 0 -- Fill with 0
 		end
 	end
 
 	if self.type == "T" then
-		self.rotation[1][2] = 1
-		self.rotation[2][1] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
+		self.grid[1][2] = 1
+		self.grid[2][1] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
 	elseif self.type == "Z" then
-		self.rotation[1][1] = 1
-		self.rotation[1][2] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
+		self.grid[1][1] = 1
+		self.grid[1][2] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
 	elseif self.type == "S" then
-		self.rotation[1][2] = 1
-		self.rotation[1][3] = 1
-		self.rotation[2][1] = 1
-		self.rotation[2][2] = 1
+		self.grid[1][2] = 1
+		self.grid[1][3] = 1
+		self.grid[2][1] = 1
+		self.grid[2][2] = 1
 	elseif self.type == "L" then
-		self.rotation[1][3] = 1
-		self.rotation[2][1] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
+		self.grid[1][3] = 1
+		self.grid[2][1] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
 	elseif self.type == "J" then
-		self.rotation[1][1] = 1
-		self.rotation[2][1] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
+		self.grid[1][1] = 1
+		self.grid[2][1] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
 	elseif self.type == "I" then
-		self.rotation[2][1] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
-		self.rotation[2][4] = 1
+		self.grid[2][1] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
+		self.grid[2][4] = 1
 	elseif self.type == "O" then
-		self.rotation[1][2] = 1
-		self.rotation[1][3] = 1
-		self.rotation[2][2] = 1
-		self.rotation[2][3] = 1
+		self.grid[1][2] = 1
+		self.grid[1][3] = 1
+		self.grid[2][2] = 1
+		self.grid[2][3] = 1
 	end
 end
 
